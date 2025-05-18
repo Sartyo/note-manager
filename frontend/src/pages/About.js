90fbe7e1760741333595b3b5cef46adef8caaa26
Sartyo <danielarv14121999@gmail.com>
2025-05-18 16:25:21 -0400

    Deleting react app from front, starting over with angular instead

--- frontend/src/pages/About.js
+++
@@ -1,2 +0,0 @@
-const About = () => <div><h2>About this app</h2><p>SPA powered by React</p></div>;
-export default About;
--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // backend URL
-});
-
-export const fetchNotes = () => api.get('/notes/');
-export const createNote = (noteData) => api.post('/notes/', noteData);
